004a9e04be472f0661c4835cc1678c2c5f760619
Venco <v3nco@v3nco.dev>
2025-05-07 20:04:57 +0200

    Added profile picture in contianer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,14 @@
 
 <div class="tl">
     <div class="box">
-
+        <div class="image-container">
+            <img src={gravatarsrc} alt="{name}'s profile picture : Basil from omori with happy emotion in combat state" width="128" height="128" class="pfp"/>
+        </div>
     </div>
 </div>
 <div class="r">
     <div class="box">
-
+        
     </div>
 </div>
 <div class="bl">
@@ -20,14 +22,21 @@
 </div>
 
 <style>
-    
+    .image-container {
+        display: flex; /* Make it a flex container */
+        justify-content: flex-start; /* Horizontal alignment - keep it left-aligned */
+        align-items: center; /* This centers content vertically */
+        width: 100%;
+        height: 100%; /* Take up full height of parent */
+    }
     .tl {
         position: absolute;
         top: 0;
         left: 0;
         width: 50%;
         height: 20%;
-        padding: 20px;
+        background-color: #494949;
+        padding: 15px;
     }
     .r {
         position: absolute;
@@ -35,7 +44,8 @@
         right: 0;
         width: 50%;
         height: 100%;
-        padding: 20px;
+        background-color: #494949;
+        padding: 15px;
     }
     .bl {
         position: absolute;
@@ -43,14 +53,25 @@
         left: 0;
         width: 50%;
         height: 80%;
-        padding: 20px;
+        background-color: #494949;
+        padding: 15px;
     }
     .box {
-        outline: 4px;
+        display: flex;
+        outline: 2px;
         outline-style: solid;
         outline-color: #9683EC;
-        background-color: #000000d3;
+        background-color: #292435d3;
+        align-items: center;
         min-width: 100%;
         min-height: 100%;
+    }
+    .pfp {
+        border: 2px solid #9683EC;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        object-fit: contain;
+        height: auto;
+        max-width: 100%;
+        margin: 20px; 
     }
 </style>
