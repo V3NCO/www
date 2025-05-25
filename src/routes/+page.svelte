4259b453d04b8a21900979df4e75bb3333ce44fc
Venco <v3nco@v3nco.dev>
2025-05-25 21:47:53 +0200

    Made Static, Added few commands, Font switched to Jetbrains Mono

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
+<!-- JS Part -->
+
+
 <script>
     import { defaultt } from './values.js';
     let name = 'Venco'; // In case i want to change my username basically everywhere i guess
     let gravatarsrc = 'https://gravatar.com/avatar/9f5b5ad2d2cd1bca67ec2702f8cbabf38bf1d10140bd7266ab15fdd4b2311fda?s=128';
 
     let termInput = '';
-    let termHistory = ["<p class='first'>Testing styles !</p> <style>.first { color: #9683EC; } .command { color: #DDD; }</style>"];
+    let termHistory = ["<p class='command'>Welcome to Venco's website, Type 'help' to list commands.</p> <style>.command { color: #DDD; }</style>"];
     let rightContent = defaultt;
 
     function handleCommand(e) {
@@ -14,13 +17,22 @@
             termInput = '';
             switch (command) {
                 case 'help':
-                    termHistory = [...termHistory, "<p class='command'>Available commands: help, about, clear</p> <style>.command { color: #DDD; }</style>"];
+                    termHistory = [...termHistory, "<p class='command'>Available commands: help, about, clear</p>"];
                     break;
                 case 'about':
-                    termHistory = [...termHistory, `<p class='about'></p> <style>.about { color: #9683EC; }</style>`];
+                    termHistory = [...termHistory, "<p class='command'></p>"];
                     break;
                 case 'clear':
-                    termHistory = ['<p class="first">Terminal Cleared. Type "help" for available commands.</p> <style>.first { color: #9683EC; } .command { color: #DDD; }</style>'];
+                    termHistory = ['<p class="command">Terminal Cleared. Type "help" for available commands.</p> <style>.command { color: #DDD; }</style>'];
+                    break;
+                case 'whoami':
+                    termHistory = [...termHistory, '<p class="command">Great question. Who are you ?</p>>'];
+                    break;
+                case 'links':
+                    termHistory = [...termHistory, '<p class="command">Here are some of my profiles on platforms!</p>'];
+                    break;
+                case 'awesome':
+                    termHistory = [...termHistory, '<p class="command">Awesome !</p> <style>keyframes Color{0%{color:#A0D468;}20%{color:#4FC1E9;}40%{color:#FFCE54;}60%{color:#FC6E51;}80%{color:#ED5565;}100%{color:#AC92EC;}}@-moz-keyframes Color{0%{color:#A0D468;}20%{color:#4FC1E9;}40%{color:#FFCE54;}60%{color:#FC6E51;}80%{color:#ED5565;}100%{color:#AC92EC;}}@-webkit-keyframes Color{0%{color:#A0D468;}20%{color:#4FC1E9;}40%{color:#FFCE54;}60%{color:#FC6E51;}80%{color:#ED5565;}100%{color:#AC92EC;}} .command{animation: Color 4s linear infinite; -webkit-animation: Color 4s ease-in-out infinite;}</style>'];
                     break;
                 default:
                     termHistory = [...termHistory, `<p class='error'>Unknown command: ${command}</p> <style>.error { color: red; }</style>`];
@@ -28,6 +40,10 @@
         }
     }
 </script>
+
+
+<!-- HTML Part -->
+
 
 <div class="tl">
     <div class="box">
@@ -38,7 +54,8 @@
 </div>
 <div class="r">
     <div class="box">
-        {@html rightContent}
+        <!-- {@html rightContent} -->
+        
     </div>
 </div>
 <div class="bl">
@@ -65,7 +82,15 @@
     </div>
 </div>
 
+
+
+
+<!-- CSS Part -->
+
+
+
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
     .image-container {
         display: flex;
         justify-content: flex-start;
@@ -128,7 +153,7 @@
         align-items: stretch;
     }
     .terminal {
-        font-family: 'Courier New', monospace;
+        font-family: 'JetBrains Mono', monospace;
         color: #ffffff;
         width: 100%;
         height: 100%;
@@ -174,7 +199,7 @@
         background: transparent;
         border: none;
         color: #DDD;
-        font-family: 'Courier New', monospace;
+        font-family: 'Jetbrains Mono', monospace;
         flex-grow: 1;
         outline: none;
         font-size: 1em;
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+    preprocess: [vitePreprocess()],
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
 export default config;
