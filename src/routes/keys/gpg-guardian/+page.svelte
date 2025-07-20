e9c4b225e64ff8a7bbf5a21b22898be18ea09329
Venco <v3nco@v3nco.dev>
2025-07-20 17:55:43 +0200

    feat: Add SSH Key generated from GPG Key

--- src/routes/keys/gpg-guardian/+page.svelte
+++ src/routes/keys/gpg-guardian/+page.svelte
@@ -12,6 +12,7 @@
     <p><strong>Key Usage :</strong> Certification, Sign, Encrypt, Authenticate</p>
     <p><strong>Keyservers :</strong> keys.openpgp.org, keyserver.ubuntu.com</p>
     <p><strong>Download :</strong> <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/6FAA8732F6512AAD6EDC522AE5E0FA4E59297845">https://keys.openpgp.org/vks/v1/by-fingerprint/6FAA8732F6512AAD6EDC522AE5E0FA4E59297845</a></p>
+    <p><strong>SSH Public Key :</strong>ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3PiNEseKdDtTl84l6I5j7Mp5zrbRAOvJtIOHfQ1eBXQ+UP4+JD9BkxedRoAO4MFt9aHGUcy1zGEf6mouStjTcMLstpIFGG6pnMMJsFqXg+2tMBePNVGloefVkPjcCekVauTZV51Rr/rQhMLaQZzjf+CicT+/P2xPbE+6RLKwmgFxc0ChAJF4giRiDkF87zGxmuQE219IbCfBTO2dl94RvMXKGUufAJWKu+vi63fXxCVje3WBj/KtntL91aZqE4pgyuis4l9kLzL5hyKeDcM4EvEyimkX4MHDO7YwJlchCRcs1wo18i6/waC3QKYKL7Kgt+BY9Th81qJ5iAwuappotp4ssEo0N8+GxebbMYP6qi7CcsleFcGQSlzmVw2q5K64uCJ+hvI1EE0jPBXCQlJAc5NQtvsR6+FYBWZ/F87i+7y8nn3dWw1KOwXKlKiPlNI5Fn6a5NtQ20tSgyiMQFRQ30ZOwsUFvgoMKTjtsQMEaqljqo0b7L0AOsTyfAXPtY2UQtTxQYzZ7G5wxJIu6eIe8Slisa1T4XHoQGaoF9MWK/v97kYnud9Cp+jhdmlckV7fwyTb2JLF8W4IDsRA7qEgb1p/ERuRarhIkXtjb1FiXF6I+tOKhEPdzy3X9zoiP+SXuE9NzchWHZ59Mr08ZRXaV0s9XkKFRU8y7VwaOc+RSSQ== openpgp:0x148001EA</p>
 </div>
 
 <style>
